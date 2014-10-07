4195180425311728eb0f3ebeb561daee79040dc1
Daniel Rocco <drocco@gmail.com>
2014-10-06 20:26:51 -0400

    initial import: zero case, test

---
+++ english_number.py
@@ -0,0 +1,6 @@
+def english_number(n):
+    return 'zero'
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_english.py
@@ -0,0 +1,5 @@
+from english_number import english_number
+
+
+def test_zero():
+    assert 'zero' == english_number(0)
